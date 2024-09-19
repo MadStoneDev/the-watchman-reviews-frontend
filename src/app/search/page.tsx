614f1fe99060b007d568f521d80af19e851bc013
Richard H. <richard@haddads.net.au>
2024-09-20 00:32:39 +1000

    search page basic

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,13 @@
 ﻿import React from "react";
+import { Input } from "@/components/ui/input";
+import SearchForm from "@/components/SearchForm";
 
 export default function SearchPage() {
   return (
     <>
       <section className={`mt-20`}>
         <h1 className={`max-w-60 text-4xl font-bold`}>Search</h1>
+        <SearchForm />
       </section>
     </>
   );
---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+
+import { Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+export default function SearchForm() {
+  return (
+    <>
+      <Input type="search" placeholder="Search" />
+    </>
+  );
+}
