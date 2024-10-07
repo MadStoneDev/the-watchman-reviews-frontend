59ad39e3846dad9d27164ab58fdea75c477f7b99
Richard H. <richard@haddads.net.au>
2024-10-07 10:40:23 +1000

    next auth setup

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,9 @@
 export { auth as middleware } from "@/auth";
+
+// export const config = {
+//   matcher: [
+//     "/((?!.*\\..*|_next).*)",
+//     "/",
+//     "/((?!.*next\\/server\\/pages-manifest.json).*)",
+//   ],
+// };
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,52 @@
-﻿import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+﻿// app/api/auth/[...nextauth]/route.js
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import GoogleProvider from "next-auth/providers/google";
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 
-export const { GET, POST } = handlers;
+const handler = NextAuth({
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // Add other providers as needed
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
+  }),
+  callbacks: {
+    async session({ session, user }) {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+    async signIn({ user }) {
+      await syncUserWithStrapi(user);
+      return true;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
+
+async function syncUserWithStrapi(user: any) {
+  const response = await fetch(
+    `${process.env.STRAPI_URL}/api/nextauth-sync/sync-user`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ user }),
+    },
+  );
+
+  if (!response.ok) {
+    console.error("Failed to sync user with Strapi");
+  }
+}
