dc152ac68644b23daa1acedc8941ff8f27041861
Richard H. <richard@haddads.net.au>
2025-03-30 23:46:37 +1000

    fix main nav

--- src/components/main-navigation.tsx
+++ src/components/main-navigation.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import Logo from "@/src/components/logo";
 import MainFooter from "@/src/components/main-footer";
@@ -39,16 +39,19 @@ export default function MainNavigation({
 }: MainNavigationProps) {
   // Hooks
   const pathname = usePathname();
+  const router = useRouter();
 
   return (
     <nav
       className={`fixed md:top-0 bottom-0 left-0 right-0 md:right-auto px-10 pb-2 pt-5 md:p-10 min-h-[70px] md:min-h-auto md:min-w-[250px] bg-neutral-800 z-50`}
     >
-      <section className={`flex flex-col h-full`}>
+      <section
+        className={`flex flex-row md:flex-col gap-10 md:gap-0 justify-center md:justify-start items-center md:items-start w-full h-full`}
+      >
         <Logo className={`hidden md:block`} />
 
         <article
-          className={`flex-grow md:mt-20 flex md:flex-col justify-between md:justify-start items-center md:items-start md:gap-14`}
+          className={`md:flex-grow md:mt-20 flex md:flex-col justify-center md:justify-start items-center md:items-start gap-10 md:gap-14`}
         >
           {items.map(({ icon, label, href }) => (
             <Link
@@ -70,12 +73,10 @@ export default function MainNavigation({
           ))}
         </article>
 
-        <article
-          className={`fixed top-5 right-5 md:right-auto md:relative flex items-center gap-2 md:w-full`}
-        >
+        <article className={`flex items-center gap-2 md:w-full`}>
           {user ? (
             <div
-              className={`flex-grow flex flex-row-reverse md:flex-row items-center gap-2`}
+              className={`flex-grow flex flex-row-reverse items-center gap-2`}
             >
               <Link
                 href={"/me"}
@@ -89,8 +90,10 @@ export default function MainNavigation({
                 onClick={async () => {
                   const supabase = createClient();
                   await supabase.auth.signOut();
+                  router.push("/");
+                  router.refresh();
                 }}
-                className={`p-2 flex items-center justify-center ${
+                className={`fixed top-5 md:top-auto right-5 md:right-auto md:relative p-2 flex items-center justify-center ${
                   pathname === "/logout"
                     ? "text-lime-400"
                     : "text-neutral-600 hover:text-lime-400"
