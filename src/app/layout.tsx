70031aed9fe7213fbb2c88f7fbd3f4b047bd1ca7
richardhaddadau <richard@haddads.net.au>
2025-11-28 22:32:47 +1000

    add mixpanel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,10 @@ import "./globals.css";
 import React from "react";
 import localFont from "next/font/local";
 
-import { ThemeProvider } from "@/src/components/theme-provider";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/src/components/providers/theme-provider";
+
+import mixpanel from "mixpanel-browser";
 
 const montserrat = localFont({
   src: "./fonts/Montserrat.ttf",
@@ -17,6 +19,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Create an instance of the Mixpanel object, your token is already added to this snippet
+  mixpanel.init('6e7486bfdb45e97af1c275b9a0a6fd45', {
+    autocapture: true,
+    record_sessions_percent: 100,
+  });
+
+  
   return (
     <html lang="en" suppressHydrationWarning>
       <body
