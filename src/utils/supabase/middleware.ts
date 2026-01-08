c2a3fa79b209dd739f7b79c11b4545f947c62d7e
Richard H. <richard@haddads.net.au>
2026-01-08 23:08:02 +1000

    fix middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -38,11 +38,24 @@ export async function updateSession(request: NextRequest) {
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
+  const publicRoutes = [
+    "/",
+    "/browse",
+    "about",
+    "/collections",
+    "/how-it-works",
+    "/search",
+    "/auth",
+    "/login",
+  ];
+
+  const isPublicRoute = publicRoutes.some(
+    (route) =>
+      request.nextUrl.pathname === route ||
+      request.nextUrl.pathname.startsWith(route + "/"),
+  );
+
+  if (!user && !isPublicRoute) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/login";
